5f0f32515460d58901704d79db458ddc3278799f
tara <y.cai@stat.ubc.ca>
2016-01-24 14:27:19 -0500

    upload plot4 and its R file

--- plot1.R
+++ plot1.R
@@ -5,8 +5,7 @@ temp$Date <- as.Date(temp$Date, format = '%d/%m/%Y')
 # only take dates in Feb, 2007.
 dat <- subset(temp, temp$Date >= '2007-02-01' & temp$Date < '2007-03-01')
 
-# Histogram
-dat$Global_active_power <- as.numeric(dat$Global_active_power)
+# histogram
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
 with(dat, hist(Global_active_power, col = "red", 
                xlab = "Global Active Power (kilowatts)", 
